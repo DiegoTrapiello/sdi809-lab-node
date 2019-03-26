74a72efd15cc687e8b8dbd5ac2c15b562d3c8a8f
DiegoTrapiello <diego_trapiello@hotmail.com>
2019-03-26 12:56:16 +0100

    “SDINodeJS-8.9 y 8.10 - Colecciones relacionadas y routers”.

--- app.js
+++ app.js
@@ -24,10 +24,47 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.use(express.static('public'));
-
 var gestorBD = require("./modules/gestorBD.js");
 gestorBD.init(app, mongo);
+
+// routerUsuarioSession
+var routerUsuarioSession = express.Router();
+routerUsuarioSession.use(function (req, res, next) {
+    console.log("routerUsuarioSession");
+    if (req.session.usuario) {
+        // dejamos correr la petición
+        next();
+    } else {
+        console.log("va a : " + req.session.destino)
+        res.redirect("/identificarse");
+    }
+});
+
+//Aplicar routerUsuarioSession
+app.use("/canciones/agregar", routerUsuarioSession);
+app.use("/publicaciones", routerUsuarioSession);
+
+//routerAudios
+var routerAudios = express.Router();
+routerAudios.use(function (req, res, next) {
+    console.log("routerAudios");
+    var path = require('path');
+    var idCancion = path.basename(req.originalUrl, '.mp3');
+    gestorBD.obtenerCanciones(
+        {id: mongo.ObjectID(idCancion)}, function (canciones) {
+            if (req.session.usuario && canciones[0].autor == req.session.usuario) {
+                next();
+            } else {
+                res.redirect("/tienda");
+            }
+        })
+});
+//Aplicar routerAudios
+app.use("/audios/", routerAudios);
+
+
+app.use(express.static('public'));
+
 // Variables
 app.set('port', 8081);
 app.set('db', 'mongodb://admin:sdi@tiendamusica-shard-00-00-3vp3x.mongodb.net:27017,tiendamusica-shard-00-01-3vp3x.mongodb.net:27017,tiendamusica-shard-00-02-3vp3x.mongodb.net:27017/test?ssl=true&replicaSet=tiendamusica-shard-0&authSource=admin&retryWrites=true');
