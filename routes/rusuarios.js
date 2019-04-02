94c017b1bc9c3a80ae2c87d59867e15eee9adcb1
DiegoTrapiello <diego_trapiello@hotmail.com>
2019-04-02 11:46:36 +0200

    “SDI-NodeJS-9.4- Mensajes y alertas”.

--- routes/rusuarios.js
+++ routes/rusuarios.js
@@ -17,9 +17,11 @@ module.exports = function (app, swig, gestorBD) {
         }
         gestorBD.insertarUsuario(usuario, function (id) {
             if (id == null) {
-                res.send("Error al insertar ");
+                res.redirect("/registrarse?mensaje=Error al registrar usuario")
+
             } else {
-                res.send('Usuario Insertado ' + id);
+                res.redirect("/identificarse?mensaje=Nuevo usuario registrado");
+
             }
         });
 
@@ -40,7 +42,9 @@ module.exports = function (app, swig, gestorBD) {
         gestorBD.obtenerUsuarios(criterio, function (usuarios) {
             if (usuarios == null || usuarios.length == 0) {
                 req.session.usuario = null;
-                res.send("No identificado: ");
+                res.redirect("/identificarse" +
+                    "?mensaje=Email o password incorrecto" +
+                    "&tipoMensaje=alert-danger ");
             } else {
                 req.session.usuario = usuarios[0].email;
                 res.redirect("/publicaciones");
